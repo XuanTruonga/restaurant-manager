3b89a89166cca1f8447c32132250b91c5ddeece2
truongmt <truongdkm2016@gmail.com>
2024-02-22 10:32:20 +0700

    add css basline

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <CssBaseline />
   </React.StrictMode>
 );
 
--- src/layout/defaultLayout.js
+++ src/layout/defaultLayout.js
@@ -1,5 +1,4 @@
-import { Fullscreen } from '@mui/icons-material';
-import { Box, Container, Grid, Stack, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import React from 'react';
 
 const DefaultLayout = () => {
